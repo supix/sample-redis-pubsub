7caed3b3af4967eaed3954c9f09eb05aabed64cf
Marcello Esposito <esposito.marce@gmail.com>
2023-02-22 22:21:47 +0100

    Load test implented and activated.
    Chat disabled.

--- RedisTrial/Program.cs
+++ RedisTrial/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
 using System.Threading;
 using StackExchange.Redis;
 
@@ -9,6 +12,12 @@ namespace RedisTrial
         static void Main(string[] args)
 
         {
+            // runChat();
+            runLoadTest();
+        }
+
+        static void runLoadTest()
+        {
             //var db = redis.GetDatabase();
             //string value = "abcdefg";
             //db.StringSet("mykey", value);
@@ -16,10 +25,59 @@ namespace RedisTrial
             //value = db.StringGet("mykey");
             //Console.WriteLine(value); // writes: "abcdefg"
 
-            var guid = Guid.NewGuid().ToString();
-            var defaultCs = "localhost:8080";
-            var prompt = "Type a message: ";
+            var redis = getConnectionMultiplexer();
 
+            var db = redis.GetDatabase();
+            var itemsToSave = 500000;
+            var itemsToLoad = 1000;
+            var entries = Enumerable
+                .Range(0, itemsToSave)
+                .Select(i => new KeyValuePair<RedisKey, RedisValue>(i.ToString(), Guid.NewGuid().ToString()))
+                .ToArray();
+            Console.WriteLine($"Saving {itemsToSave} elements...");
+            var sw = new Stopwatch();
+            sw.Start();
+            db.StringSet(entries);
+            sw.Stop();
+            Console.WriteLine($"Saved in {sw.ElapsedMilliseconds} ms.");
+            Thread.Sleep(3000);
+
+            var keys = new List<RedisKey>();
+            var rnd = new Random();
+            while (keys.Count() < itemsToLoad)
+            {
+                var key = rnd.Next(itemsToSave).ToString();
+                if (keys.IndexOf(key) < 0)
+                    keys.Add(key);
+            }
+            Console.WriteLine($"Loading {itemsToLoad} elements...");
+            sw.Reset();
+            sw.Start();
+            var values = db.StringGet(keys.ToArray());
+            sw.Stop();
+            Console.WriteLine($"Loaded in {sw.ElapsedMilliseconds} ms.");
+
+            //for (int i = 0; i < keys.Count(); i++)
+            //{
+            //    var expectedValue = entries.Single(kv => kv.Key == keys[i]).Value;
+            //    var valueGot = values[i];
+                
+            //    if (expectedValue != valueGot)
+            //        Console.WriteLine($"Error! Expected: {expectedValue}. Got: {valueGot}");
+            //    //else
+            //    //    Console.WriteLine($"Ok! {i.ToString()}: {expectedValue}");
+            //}
+
+            sw.Reset();
+            sw.Start();
+            db.KeyDelete(keys.ToArray());
+            sw.Stop();
+            Console.WriteLine($"Keys deleted in {sw.ElapsedMilliseconds} ms.");
+        }
+
+        private static ConnectionMultiplexer getConnectionMultiplexer()
+        {
+            var defaultCs = "localhost:8080";
 
             Console.Write($"Insert connection string [{defaultCs}]: ");
             var cs = Console.ReadLine();
@@ -29,6 +87,15 @@ namespace RedisTrial
             Console.WriteLine("Connecting...");
             var redis = ConnectionMultiplexer.Connect(cs);
             Console.WriteLine("Connected to " + cs);
+            return redis;
+        }
+
+        static void runChat()
+        {
+            var guid = Guid.NewGuid().ToString();
+            var prompt = "Type a message: ";
+
+            var redis = getConnectionMultiplexer();
 
             ISubscriber sub = redis.GetSubscriber();
             sub.Subscribe("messages", (channel, message) => {
